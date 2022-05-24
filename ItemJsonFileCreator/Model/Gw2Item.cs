2a2ae8400ffcc6876884bbdd589f8f31cf82dae0
Taschenbuch <43114787+Taschenbuch@users.noreply.github.com>
2022-05-24 21:59:11 +0200

    add itemJsonFileCreator project

---
+++ ItemJsonFileCreator/Model/Gw2Item.cs
@@ -0,0 +1,12 @@
+﻿namespace ItemJsonFileCreator.Model
+{
+    public class Gw2Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+        public string Rarity { get; set; }
+        public Details Details { get; set; } = new Details();
+    }
+}
